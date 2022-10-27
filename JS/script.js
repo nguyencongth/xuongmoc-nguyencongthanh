d224f70396244beade0c49c7fcbf4efae244fdb7
Nguyen Cong Thanh <thanhproe2k2@gmail.com>
2022-10-27 21:54:11 +0700

    upload source code

--- JS/script.js
+++ JS/script.js
@@ -8,3 +8,24 @@ $(document).ready(function(){
         }
     });
 });
+
+// function submit() {
+//     var warning = document.querySelector('warning');
+//     var phoneNumber = document.getElementById('phoneNumber');
+//     var filter =  /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
+//     if (phoneNumber.value.match(filter)) {
+//         warning.innerText = "bạn nhập đúng định dạng";
+//     }
+//     else {
+//         warning.innerText = "bạn nhập k đúng định dạng";
+//     }
+// }
+// function is_phonenumber(phonenumber) {
+//     var phoneno = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
+//     if(phonenumber.match(phoneno)) {
+//       return true;
+//     }  
+//     else {  
+//       return false;
+//     }
+//   }
